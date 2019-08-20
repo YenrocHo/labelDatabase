d1d5058d8828b0aa61da842c065ad34ef35d27cd
luanbing <17621035211@163.com>
2019-08-20 17:11:28 +0800

    123

---
+++ src/main/java/com/fc/aden/vo/ProductFoodStoreVO.java
@@ -0,0 +1,32 @@
+package com.fc.aden.vo;
+
+public class ProductFoodStoreVO {
+
+    private int product = 0;
+    private int foodName = 0;
+    private int store = 0;
+
+    public int getProduct() {
+        return product;
+    }
+
+    public void setProduct(int product) {
+        this.product = product;
+    }
+
+    public int getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(int foodName) {
+        this.foodName = foodName;
+    }
+
+    public int getStore() {
+        return store;
+    }
+
+    public void setStore(int store) {
+        this.store = store;
+    }
+}
