911ba955ea05dc773881ae600d078c18344f2f9b
luanbing <17621035211@163.com>
2019-11-18 11:09:11 +0800

    修改时间不对问题

--- src/main/java/com/fc/aden/service/impl/PrintHistoryServiceImpl.java
+++ src/main/java/com/fc/aden/service/impl/PrintHistoryServiceImpl.java
@@ -5,13 +5,10 @@ import com.fc.aden.mapper.auto.process.PrintHistoryMapper;
 import com.fc.aden.model.custom.process.PrintHistory;
 import com.fc.aden.model.custom.process.PrintHistoryExample;
 import com.fc.aden.service.PrintHistoryService;
-import com.fc.aden.util.DateUtils;
-import com.fc.aden.util.IDGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -31,15 +28,9 @@ public class PrintHistoryServiceImpl extends BaseServiceImpl<PrintHistoryMapper,
      * @param printHistoryList
      * @author Created by zc on 2019/7/3
      */
-        @Override
-        @Transactional
+    @Override
+    @Transactional
     public int insertBatch(List<PrintHistory> printHistoryList) {
-        Date currentDate = new Date();
-        // 添加创建时间和更新时间
-        for (PrintHistory printHistory : printHistoryList) {
-            printHistory.setCreateTime(currentDate);
-            printHistory.setUpdateTime(currentDate);
-        }
         return printHistoryMapper.insertBatch(printHistoryList);
     }
 }
